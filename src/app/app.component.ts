919ab5bfccff9602963ffacc549506a9f9ecdd40
Amit Rozen <amitrozen2015@gmail.com>
2024-07-15 19:57:53 +0300

    done

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { QrComponent } from './qr/qr.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, QrComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'QR-code';
+}
